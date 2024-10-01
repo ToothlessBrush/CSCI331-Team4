8d2ef68228efb474c85a3aaaf58e8af635a868b6
ToothlessBrush <johnklein514@gmail.com>
2024-10-01 12:34:58 -0500

    buffer header created

---
+++ Project2/src/buffer.h
@@ -0,0 +1,19 @@
+#include <string>
+#include <map>
+#include <vector>
+
+struct ZipCodeData
+{
+    int zip_code;
+    std::string place_name;
+    std::string state;
+    std::string county;
+    float latitude;
+    float longitude;
+};
+
+class Buffer
+{
+private:
+public:
+};
