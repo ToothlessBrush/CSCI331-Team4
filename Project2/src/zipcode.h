9530e65f43382801054a35e86959dee661784f0d
Joshua Olaoye <joshuaolaoye@Joshuas-Laptop.lan>
2024-10-01 23:19:26 -0500

    fix: Create seperate files for Zipcode struct

---
+++ Project2/src/zipcode.h
@@ -0,0 +1,14 @@
+#ifndef ZIP_CODE_H
+#define ZIP_CODE_H
+
+struct ZipCodeData
+{
+    int zip_code;
+    std::string place_name;
+    std::string state;
+    std::string county;
+    float latitude;
+    float longitude;
+};
+
+#endif // ZIP_CODE_H
