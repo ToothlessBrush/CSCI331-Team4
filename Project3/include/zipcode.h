bc180e277df8e843e6d1c277956fa570606a5bc3
Joshua Olaoye <joshuaolaoye@Joshuas-Laptop.local>
2024-10-28 01:59:35 -0500

    refactor: Change zip code data type to string

---
+++ Project3/include/zipcode.h
@@ -0,0 +1,39 @@
+/**
+ * @file zipcode.h
+ */
+#ifndef ZIP_CODE_H
+#define ZIP_CODE_H
+
+#include <string>
+#include <tuple>
+
+/**
+ * @brief The ZipCodeData struct holds data for a single zip code, including its coordinates and place information.
+ */
+struct ZipCodeData
+{
+    std::string zip_code;     ///< The zip code.
+    std::string place_name;   ///< The name of the place corresponding to the zip code.
+    std::string state;        ///< The state (two-character abbreviation).
+    std::string county;       ///< The county of the place.
+    float latitude;           ///< The latitude coordinate.
+    float longitude;          ///< The longitude coordinate.
+
+    /**
+     * @brief Constructs a ZipCodeData object from a tuple containing zip code details.
+     * 
+     * @param tuple A tuple containing zip code, place name, state, county, latitude, and longitude.
+     */
+    ZipCodeData(std::tuple<std::string, std::string, std::string, std::string, float, float>);
+    
+    /**
+     * @brief Overloads the << operator to print the ZipCodeData to an output stream.
+     * 
+     * @param outputstream The output stream.
+     * @param zipcode The ZipCodeData object to be printed.
+     * @return The output stream after the zip code data is written.
+     */
+    friend std::ostream& operator<<(std::ostream&, const ZipCodeData&);
+};
+
+#endif // ZIP_CODE_H
---
+++ Project3/src/buffer.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file buffer.cpp
+ */
+#include "buffer.h"
+
+#include <sstream>
+
+Buffer::Buffer(const std::string& file) : reader(FileReader(file))
+{
+    this->populate_zipcodes();
+}
+
+std::tuple<std::string, std::string, std::string, std::string, float, float> Buffer::tokenize_line(const std::string& line)
+{
+    std::string zip_code;
+    std::string place_name;
+    std::string state;
+    std::string county;
+    float latitude;
+    float longitude;
+
+    std::stringstream stream(line);
+    std::string token;
+
+    std::getline(stream, token, ',');
+    zip_code = std::stoi(token);
+
+    std::getline(stream, place_name, ',');
+
+    std::getline(stream, state, ',');
+
+    std::getline(stream, county, ',');
+
+    std::getline(stream, token, ',');
+    latitude = std::stof(token);
+
+    std::getline(stream, token, ',');
+    longitude = std::stof(token);
+
+
+    return std::make_tuple(zip_code, place_name, state, county, latitude, longitude);
+}
+
+void Buffer::populate_zipcodes()
+{
+    std::vector<std::string> data = reader.get_lines();
+
+    for (int i = 1; i < data.size(); i++) {
+        std::tuple<std::string, std::string, std::string, std::string, float, float> zipcode_data = tokenize_line(data[i]);
+        this->zipcodes.push_back(ZipCodeData(zipcode_data));
+    }
+}
+
+std::vector<ZipCodeData> Buffer::get_zipcodes() {
+    return this->zipcodes;
+}
